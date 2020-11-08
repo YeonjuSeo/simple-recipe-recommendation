142bc208b74a12bf165907c68dd40984ec94dbbe
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-11-08 23:59:06 +0900

    Update app.js
    createRecipesList

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-fetch("https://www.data.go.kr/data/15057205/openapi.do")
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("Fetch Error", err);
-  });
+// Fetch the items from the JSON file
+function loadRecipes() {
+  return fetch("recipes.json")
+    .then((response) => response.json())
+    .then((json) => json.recipes);
+}
+
+function createElement(recipe) {
+  const img = document.createElement("img");
+  img.setAttribute("class", "thumbnail");
+  img.setAttribute("src", recipe.IMG_URL);
+
+  const span = document.createElement("span");
+  span.setAttribute("class", "description");
+  span.innerText = `${recipe.RECIPE_NM_KO}
+   ${recipe.SUMRY}`;
+
+  const li = document.createElement("li");
+  li.setAttribute("class", "recipe");
+  li.setAttribute("recipe__level", recipe.LEVEL_NM);
+  li.setAttribute("recipe__type", recipe.TY_NM);
+
+  li.append(img);
+  li.append(span);
+
+  return li;
+}
+
+loadRecipes().then((data) => {
+  const elements = data.map(createElement);
+  const container = document.querySelector(".recipes");
+  container.append(...elements);
+});
